6bceadf02964d1333f764172cf7fb72ed77effb9
yhpp <angchen2@outlook.com>
2023-12-18 18:05:20 +0800

    实现商品类型接口

---
+++ src/Infrastructure/Data/CatalogContextSeed.cs
@@ -0,0 +1,62 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class CatalogContextSeed
+    {
+        public static async Task SeedAsync(CatalogContext context, ILogger logger, int retry = 0)
+        {
+            var retryForAvailability = retry;
+            try
+            {
+                if (context.Database.IsSqlServer())
+                {
+                    context.Database.Migrate();
+                }
+                if (!await context.CatalogBrands.AnyAsync())
+                {
+                    await context.CatalogBrands.AddRangeAsync(GetPreconfiguredCatalogBrands());
+                    await context.SaveChangesAsync();
+                }
+                if (!await context.CatalogTypes.AnyAsync())
+                {
+                    await context.CatalogTypes.AddRangeAsync(GetPreconfiguredCatalogTypes());
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (retryForAvailability >= 10) throw;
+                retryForAvailability++;
+                logger.LogError(ex.Message);
+                await SeedAsync(context, logger, retryForAvailability);
+                throw;
+            }
+        }
+
+        static  IEnumerable<CatalogType> GetPreconfiguredCatalogTypes()
+        {
+            return new List<CatalogType>()
+            {
+                new ("T-Shirt"),
+                new("Sheet")
+            };
+        }
+
+        static IEnumerable<CatalogBrand> GetPreconfiguredCatalogBrands()
+        {
+            return new List<CatalogBrand>()
+            {
+                new ("Azure"),
+                new(".NET")
+            };
+        }
+    }
+}
